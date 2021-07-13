beed5ef5b64148b9da6bb29d26ee4e98f8cc1be7
rfa73amorim1 <rfa73amorim1@gmail.com>
2021-07-12 22:22:51 -0300

    commit apiDioJogos

---
+++ ApiDioJogos/Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using ApiDioJogos.InputModel;
+using ApiDioJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDioJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, decimal preco);
+        Task Remover(Guid id);
+    }
+}
